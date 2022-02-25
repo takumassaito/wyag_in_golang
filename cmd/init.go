d5c15b5b96479a6cd5d9fdb5e3db0eb2bd457d98
Huruido-13 <casterfox@outlook.com>
2022-02-25 23:58:36 +0900

    second attempt

--- cmd/init.go
+++ cmd/init.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/bigkevmcd/go-configparser"
 	"github.com/spf13/cobra"
@@ -19,32 +20,38 @@ type initStruct struct {
 	conf   *configparser.ConfigParser
 }
 
-func (init initStruct) New(path string) {
+func New(path string, init initStruct) *initStruct {
 	init.path = path
-	init.gitdir = path + "/.git"
+	init.gitdir = filepath.Join(path, ".git")
 
-	//Read configuration file in .git/config
-	cf := repo_file(init, "config")
+	//.git/config パスをリターン　cf = .git/config
+	cf := repo_file(init, "config", false)
 	_, Is_cf := os.Stat(cf)
+	//configがあれば読み取る
 	if Is_cf == nil {
 		init.conf, _ = configparser.NewConfigParserFromFile(cf)
 	} else {
 		//コンフィグファイルが見つからない場合エラー停止する
-		log.Fatal(Is_cf)
+		// log.Fatal(Is_cf)
+		fmt.Println("configを読み取れませんでした")
 	}
+
+	return &init
 }
-func repo_file(init initStruct, path string) string {
+func repo_file(init initStruct, path string, mkdir bool) string {
 	//create dirname(path) if absent.
-	dir_result, _ := repo_dir(init, path)
-	if dir_result {
+	dir_bool, _ := repo_dir(init, path, mkdir)
+	if dir_bool {
 		return repo_path(init, path)
+	} else {
+		//path先がディレクトリではない場合エラーで停止する
+		log.Fatal(path)
+		return path
 	}
-	//path先がディレクトリではない場合エラーで停止する
-	log.Fatal(path)
-	return path
+
 }
 
-func repo_dir(init initStruct, path string) (bool, string) {
+func repo_dir(init initStruct, path string, mkdir bool) (bool, string) {
 	//mkdir path if absent if mkdir
 	path = repo_path(init, path)
 
@@ -58,13 +65,47 @@ func repo_dir(init initStruct, path string) (bool, string) {
 			return false, "Not a directory" + path
 		}
 	}
-
-	_ = os.MkdirAll(path, 0777)
+	//mkdir=falseならばディレクトリを作らずパスのみリターン
+	if mkdir {
+		_ = os.Mkdir(path, 0777)
+	}
 	return true, path
+
 }
 
 func repo_path(init initStruct, path string) string {
-	return init.gitdir + "/" + path
+	//.gitディレクトリ配下に渡されたpath名でディレクトリを作成
+	return filepath.Join(init.gitdir, path)
+}
+
+func repo_create(path string) {
+	/*引数に構造体を受け、その構造体を利用して他の構造体を初期化する関数になってしまっているため
+	一つの構造体を初期化するのに2つも構造体を作らないといけなくなっている。要修正*/
+	var repo initStruct
+	repoed := New(path, repo)
+
+	repo_info, err := os.Stat(repoed.path)
+	//Argsに渡されたPATHが撮っている時
+	if err == nil {
+		//PATHが見つかったがディレクトリではない時
+		if !repo_info.IsDir() {
+			fmt.Printf("%s is not a directory", repoed.path)
+			log.Fatal(path)
+		}
+		_, err := filepath.Glob(repo.path + "/*")
+		//ディレクトリ内にファイルが見つかった時
+		if err != nil {
+			fmt.Printf("%s is not Empty!", repoed.path)
+			log.Fatal(path)
+		}
+		//PATHが通っていない時
+	} else {
+		//PATHにディレクトリ作成
+		os.Mkdir(repoed.path, 0777)
+		fmt.Println(repoed.path)
+		fmt.Println("コマンドライン引数に渡されたパスにディレクトリを作成しました")
+	}
+
 }
 
 // initCmd represents the init command
@@ -78,10 +119,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		//構造体の宣言
-		var test initStruct
-		test.New(os.Args[2])
-
+		repo_create(os.Args[2])
 		fmt.Println("init called")
 	},
 }
